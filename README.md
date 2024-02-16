07c4ad6810ae5ef0ac59afd93a3717e9ecbfaf4e
hackezar <107069171+hackezar@users.noreply.github.com>
2024-02-16 08:39:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Webpack_ESlist_Prettier_template
+
+A template for new projects. This template contains webpack, eslist, and prettier
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+
+// Below requires the plugins to minimize css
+// eslint-disable-next-line import/no-extraneous-dependencies
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+    /* For webpack@5 you can use the `...` syntax to extend existing minimizers
+         (i.e. `terser-webpack-plugin`), uncomment the next line */
+      '...',
+      new CssMinimizerPlugin(),
+    ],
+  },
+};
